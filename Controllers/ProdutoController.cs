bc9be9fd92a4d580b1835c95eb446537c4a0d9b0
unknown <fe97964@gmail.com>
2020-07-08 10:17:01 -0300

    Controller feito

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+using Aula34MVCConsole.Models;
+using Aula34MVCConsole.Views;
+
 namespace Aula34MVCConsole.Controllers
 {
     public class ProdutoController
     {
-        
+        Produto produtoModel = new Produto();
+        ProdutoView produtoView = new ProdutoView();
+
+        public void Listar()
+        {
+            List<Produto> lista = produtoModel.Ler();
+            produtoView.MostrarNoConsole(lista);
+        }
+
+        public void Buscar(string termo){
+            List<Produto> lista = produtoModel.Ler().FindAll(x => x.Preco == float.Parse(termo));
+            produtoView.MostrarNoConsole(lista);
+        }
     }
 }
--- Views/ProdutoView.cs
+++ Views/ProdutoView.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using Aula34MVCConsole.Models;
+
 namespace Aula34MVCConsole.Views
 {
     public class ProdutoView
