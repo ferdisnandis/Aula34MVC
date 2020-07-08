28d0caf6512a8b21128cc24cb587b6c83dd9d6e4
unknown <fe97964@gmail.com>
2020-07-08 10:06:20 -0300

    View Feito

--- Views/ProdutoView.cs
+++ Views/ProdutoView.cs
@@ -2,6 +2,12 @@ namespace Aula34MVCConsole.Views
 {
     public class ProdutoView
     {
-        
+        public void MostrarNoConsole(List<Produto> lista)
+        {
+            foreach (Produto item in lista)
+            {
+                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
+        }
     }
 }
